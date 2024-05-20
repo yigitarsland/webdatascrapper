a354d04130cf3f7b9f24517786e10b8b4fc5454f
yigitarsland <yigitarsland@icloud.com>
2024-05-20 09:26:46 +0200

    Change method to read config file Add example recipient Add condition to run only if there's a --mail arg

--- app9.py
+++ app9.py
@@ -1,4 +1,4 @@
-import configparser
+import json
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import smtplib
@@ -7,10 +7,10 @@ from datetime import datetime
 
 
 # Function to send e-mail using the university mail
-def sendEmail(subject, body, config = 'config.json'):
+def sendEmail(emailSubject, body, config = 'config.json'):
     # Read config file
-    config = configparser.ConfigParser()
-    config.read(config)
+    with open(config, 'r') as file:
+        config = json.load(file)
 
     username = config['username']
     password = config['password']
@@ -19,7 +19,8 @@ def sendEmail(subject, body, config = 'config.json'):
     # Create e-mail message
     msg = MIMEMultipart()
     msg['From'] = username
-    msg['Subject'] = subject
+    msg['To'] = 'wojciech.thomas@pwr.edu.pl' 
+    msg['Subject'] = emailSubject
 
     # Attach e-mail body
     msg.attach(MIMEText(body, 'plain'))
@@ -39,7 +40,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Send an e-mail to your teacher.')
     parser.add_argument('--mail', type=str, required=True, help='The content of the e-mail')
     args = parser.parse_args()
-    emailBody = args.mail
-    currentdatetime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    emailSubject = f'Message sent on {currentdatetime}'
-    sendEmail(emailSubject, emailBody)
+    if args.mail:
+        emailBody = args.mail # Get the content of the --mail argument
+        currentdatetime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        emailSubject = f'Message sent on {currentdatetime}'
+        sendEmail(emailSubject, emailBody)
